acfbd6b71d9da9d3392bcbaa71375ad185fa824e
nathan <nathan.kwok3@edg.com.au>
2024-10-20 01:18:08 +1100

    initialize ai saas business

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,6 @@
+const DashboardPage = () => {
+  // ** Only authorised Users can see the dashboard page **//
+  return <p>Dashboard Page (Protected)</p>;
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,6 @@
+const LandingPage = () => {
+  // ** Every user will see this landing page if they are logged in or not**//
+  return <div>Landing Page (Unprotected)</div>;
+};
+
+export default LandingPage;
